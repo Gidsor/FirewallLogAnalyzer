04e2c7bd4bd98d2e2e4e306b16729ed3eb1725df
Vadim Denisov <gidsor@gmail.com>
2018-07-12 14:20:24 +0500

    new rule for gitignore

--- src/main/kotlin/com/gidsor/firewalllog/FirewallLogApp.kt
+++ src/main/kotlin/com/gidsor/firewalllog/FirewallLogApp.kt
@@ -3,9 +3,11 @@ package com.gidsor.firewalllog
 import tornadofx.*
 
 class FirewallLogApp : App(MyView::class) {
+    companion object {
+        fun main(args: Array<String>) {
+            launch<FirewallLogApp>(args)
+        }
+    }
 
-}
 
-fun main(args: Array<String>) {
-    launch<FirewallLogApp>(args)
 }
