413d34ad6b70ab696166e93f871b52bdff594e21
Vadim Denisov <gidsor@gmail.com>
2018-07-13 16:24:09 +0500

    add styles

--- src/main/kotlin/com/gidsor/firewalllog/app/FirewallLogApp.kt
+++ src/main/kotlin/com/gidsor/firewalllog/app/FirewallLogApp.kt
@@ -4,7 +4,7 @@ import com.gidsor.firewalllog.views.MainView
 import javafx.application.Application
 import tornadofx.*
 
-class FirewallLogApp : App(MainView::class)
+class FirewallLogApp : App(MainView::class, Styles::class)
 
 fun main(args: Array<String>) {
     Application.launch(FirewallLogApp::class.java, *args)
