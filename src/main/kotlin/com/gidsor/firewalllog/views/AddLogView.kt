a772f2743a7264e4a2843615171a6d78dd7ef46d
Vadim Denisov <gidsor@gmail.com>
2018-07-14 09:15:46 +0500

    default value for combobox

--- src/main/kotlin/com/gidsor/firewalllog/views/AddLogView.kt
+++ src/main/kotlin/com/gidsor/firewalllog/views/AddLogView.kt
@@ -2,23 +2,27 @@ package com.gidsor.firewalllog.views
 
 import com.gidsor.firewalllog.controllers.StoreKaspersky
 import com.gidsor.firewalllog.utils.FirewallType
+import javafx.beans.property.SimpleObjectProperty
 import javafx.collections.FXCollections
 import javafx.stage.FileChooser
 import tornadofx.*
 
 class AddLogView : View("My View") {
     val firewalls = FXCollections.observableArrayList<FirewallType>()
+    val firstFirewall: SimpleObjectProperty<FirewallType>
 
     val storeKaspersky: StoreKaspersky by inject()
 
     init {
         firewalls.addAll(FirewallType.values())
+        firstFirewall = SimpleObjectProperty<FirewallType>(firewalls.first())
     }
 
     override val root = vbox {
         label("Выберите межсетевой экран")
 
-        combobox(values = firewalls) {
+        combobox(firstFirewall, firewalls) {
+
         }
 
         label("Выберите лог-файл")
