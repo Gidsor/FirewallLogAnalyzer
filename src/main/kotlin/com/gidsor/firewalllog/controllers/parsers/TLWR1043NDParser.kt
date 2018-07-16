395e9fe31ba628caf69ec63d3d41b50ab8d28be3
Vadim Denisov <gidsor@gmail.com>
2018-07-16 14:26:25 +0500

    parse date of logs for TLWR1043ND

--- src/main/kotlin/com/gidsor/firewalllog/controllers/parsers/TLWR1043NDParser.kt
+++ src/main/kotlin/com/gidsor/firewalllog/controllers/parsers/TLWR1043NDParser.kt
@@ -1,8 +1,12 @@
 package com.gidsor.firewalllog.controllers.parsers
 
 import com.gidsor.firewalllog.models.logfiles.template.TLWR1043NDLogFile
+import tornadofx.*
 import java.time.LocalDate
 import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoField
+import java.util.*
 
 class TLWR1043NDParser {
     companion object {
@@ -10,11 +14,18 @@ class TLWR1043NDParser {
             val logs = mutableListOf<TLWR1043NDLogFile>()
 
             val textsplit = text.split(",")
+            val year = textsplit[3].substring(10, 14)
+
             for (t in textsplit.subList(10, textsplit.size)) {
                 val s = t.split("\t")
 
-                val date: LocalDate = LocalDate.now()
-                val time: LocalTime = LocalTime.now()
+                // MMM dd
+                val md = s[0].substring(1, s[0].length - 8).split(" ")
+                val month = DateTimeFormatter.ofPattern("MMM").withLocale(Locale.ENGLISH).parse(md[0]).get(ChronoField.MONTH_OF_YEAR)
+                val hms = s[0].substring(s[0].length - 8 ,s[0].length).split(":")
+
+                val date: LocalDate = LocalDate.of(year.toInt(), month, md[1].toInt())
+                val time: LocalTime = LocalTime.of(hms[0].toInt(), hms[1].toInt(), hms[2].toInt())
                 val type: String = s[1]
                 val level: String = s[2]
                 val logContent: String = s[3]
